95102a47a1dbad5a709d04c2dc3a5a148e8cce7f
marcos2504 <marcosibarra1234@gmail.com>
2022-10-12 11:00:06 -0300

    intregrado

--- tests.py
+++ tests.py
@@ -5,10 +5,10 @@ class TestOrdenarResultado(unittest.TestCase):
       resultado=OrdenarResultado([5,6,2,1,8])
       self.assertEqual(resultado,[1,2,5,6,8])
     def  test_2(self) :
-      resultado=OrdenarResultado([7,6,3,2,8])
-      self.assertEqual(resultado,[2,3,6,7,8])
+      resul=OrdenarResultado([7,6,3,2,8])
+      self.assertEqual(resul,[2,3,6,7,8])
     def  test_3(self) :
-      resultado=OrdenarResultado([5,2,2,1,8])
-      self.assertEqual(resultado,[1,2,2,5,8])
+      r=OrdenarResultado([5,2,2,1,8])
+      self.assertEqual(r,[1,2,2,5,8])
 if __name__ == '__main__':
     unittest.main()
